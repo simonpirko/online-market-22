2fc5c90c1e026baf94b7b59633ae4ecb26cda0af
Irina-Kr <133982815+Irina-Kr@users.noreply.github.com>
2023-11-28 14:38:02 +0300

    UserRegistrationAndLoginDto

---
+++ src/main/java/by/tms/dto/LoginUserDto.java
@@ -0,0 +1,36 @@
+package by.tms.dto;
+
+
+import by.tms.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
+
+
+import javax.transaction.Transactional;
+
+@Getter
+@Setter
+@ToString
+public class LoginUserDto {
+
+    @NotEmpty
+    @NotBlank
+    private String emailAddress;
+
+    @NotEmpty
+    @NotBlank
+    private String phoneNumber;
+
+    @NotEmpty
+    @NotBlank
+    @Range(min = 2,max = 16)
+    private String password;
+    public LoginUserDto(){}
+}
+
+
+
---
+++ src/main/java/by/tms/dto/RegistrationUserDto.java
@@ -0,0 +1,36 @@
+package by.tms.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
+
+import javax.management.relation.Role;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Getter
+@Setter
+@ToString
+public class RegistrationUserDto {
+    @NotEmpty
+    @NotBlank
+    private String emailAddress;
+
+    @NotEmpty
+    @NotBlank
+    private String phoneNumber;
+
+    @NotEmpty
+    @NotBlank
+    @Range(min = 2,max = 16)
+    private String password;
+
+
+    public  RegistrationUserDto(){}
+}
+
+
+
