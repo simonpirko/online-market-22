04a6bba479c83203ed1887d281d80fbdcd43eb35
ivan <ivandolgolaptev@gmail.com>
2023-11-28 08:07:12 +0300

    fix PromotionService

--- src/main/java/by/tms/service/PromotionService.java
+++ src/main/java/by/tms/service/PromotionService.java
@@ -5,9 +5,11 @@ import by.tms.entity.Promotion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class PromotionService {
 
     @Autowired
