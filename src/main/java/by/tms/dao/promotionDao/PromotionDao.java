7554d7e4e849121822b8f3ea92cd456469411007
ivan <ivandolgolaptev@gmail.com>
2023-11-30 13:46:27 +0300

    fix: PromotionDao && PromotionService

--- src/main/java/by/tms/dao/promotionDao/PromotionDao.java
+++ src/main/java/by/tms/dao/promotionDao/PromotionDao.java
@@ -2,16 +2,21 @@ package by.tms.dao.promotionDao;
 
 import by.tms.dao.Dao;
 import by.tms.entity.Promotion;
+import by.tms.entity.Shop;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Transactional
 public class PromotionDao implements Dao<Promotion, Long> {
+    private final static String FIND_ALL = "SELECT pro FROM tb_promotion pro";
+    private final static String DELETE_BY_ID = "DELETE FROM tb_promotion pro WHERE pro.id = :id";
 
     private final SessionFactory sessionFactory;
 
@@ -22,31 +27,56 @@ public class PromotionDao implements Dao<Promotion, Long> {
 
     @Override
     public Promotion save(Promotion promotion) {
-        return null;
+        Session session = sessionFactory.openSession();
+        session.save(promotion);
+        session.close();
+        return promotion;
     }
 
     @Override
     public Optional<Promotion> findById(Long id) {
-        return Optional.empty();
+        Session session = sessionFactory.openSession();
+        Promotion promotion = session.get(Promotion.class, id);
+        session.close();
+
+        if (promotion != null) {
+            return Optional.of(promotion);
+        } else {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Promotion> findAll() {
-        return null;
+        List<Promotion> promotionList = new ArrayList<>();
+       Session session = sessionFactory.openSession();
+
+        session.createQuery(FIND_ALL, Promotion.class)
+               .getResultList();
+       session.close();
+
+       return promotionList;
     }
 
     @Override
     public void remove(Promotion promotion) {
-
+        Session session = sessionFactory.openSession();
+        session.remove(promotion);
+        session.close();
     }
 
     @Override
     public void removeById(Long id) {
-
+        Session session = sessionFactory.openSession();
+        session.createQuery(DELETE_BY_ID, Shop.class)
+               .setParameter("id", id)
+               .executeUpdate();
+        session.close();
     }
 
     @Override
     public void update(Promotion promotion) {
-
+        Session session = sessionFactory.openSession();
+        session.update(promotion);
     }
 }
